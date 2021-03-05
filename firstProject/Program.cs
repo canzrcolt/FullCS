38a50244fe1d35560d19c1b1cc938b6949a357ad
canzrcolt <canzrcolt@gmail.com>
2021-03-05 18:23:31 -0500

    just making sure everything is pushed

---
+++ firstProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using firstProject.math;
+
+namespace firstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nummy = 255;
+            nummy += 1;
+            var number = "12345";
+            int i = Convert.ToInt32(number);
+            System.Console.WriteLine($"{number.GetType()} {i.GetType()} {nummy}");
+            var sillyAdd = new Calculator();
+            System.Console.WriteLine(sillyAdd.add(2, 17));
+        }
+
+
+    }
+}
